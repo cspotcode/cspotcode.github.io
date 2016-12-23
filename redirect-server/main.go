d4830b590dbbb1f1b61403d678b9620bf6004690
Andrew Bradley <cspotcode@gmail.com>
2016-12-23 15:30:02 -0500

    Fixes redirection behavior

--- redirect-server/main.go
+++ redirect-server/main.go
@@ -4,11 +4,24 @@ import (
     "net/http"
 )
 
+var sourceDomain string
+var targetDomain string
+
 func init() {
     http.HandleFunc("/", redirect)
+    sourceDomain = "www.cspotcode.com"
+    targetDomain = "cspotcode.com"
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "https://cspotcode.com/" + r.URL.RawPath + r.URL.RawQuery, http.StatusMovedPermanently)
+    if(r.Host == sourceDomain) {
+        redirectTo := "https://" + targetDomain + r.URL.Path
+        if(r.URL.RawQuery != "") {
+            redirectTo += "?" + r.URL.RawQuery
+        }
+        http.Redirect(w, r, redirectTo, http.StatusMovedPermanently)
+        return
+    }
+    http.Error(w, "", http.StatusNotFound)
 }
 
