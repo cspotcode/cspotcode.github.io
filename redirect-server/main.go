2f85662578e989fc1ec701ddfcf07983126aba8f
Andrew Bradley <cspotcode@gmail.com>
2016-12-23 15:42:22 -0500

    Pulls source and target domains from DataStore

--- redirect-server/main.go
+++ redirect-server/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
     "net/http"
+    "google.golang.org/appengine"
+    "google.golang.org/appengine/datastore"
 )
 
-var sourceDomain string
-var targetDomain string
+type Redirect struct {
+    sourceDomain string
+    targetDomain string
+}
+
+var redirect Redirect
 
 func init() {
     http.HandleFunc("/", redirect)
-    sourceDomain = "www.cspotcode.com"
-    targetDomain = "cspotcode.com"
+}
+
+func getRedirect(r *http.Request) {
+    if(redirect != nil) {
+        return
+    }
+    ctx := appengine.NewContext(r)
+    redirect = new(Redirect)
+    k := datastore.NewKey(ctx, "redirect", "singleton", 0, nil)
+    datastore.Get(ctx, k, redirect)
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-    if(r.Host == sourceDomain) {
-        redirectTo := "https://" + targetDomain + r.URL.Path
+    getRedirect(r)
+    if(r.Host == redirect.sourceDomain) {
+        redirectTo := "https://" + redirect.targetDomain + r.URL.Path
         if(r.URL.RawQuery != "") {
             redirectTo += "?" + r.URL.RawQuery
         }
