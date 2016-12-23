62a66ce91e3d58ce86bb55d9399949e3e563d708
Andrew Bradley <cspotcode@gmail.com>
2016-12-23 14:24:13 -0500

    Fixes syntax errors in go code

--- redirect-server/main.go
+++ redirect-server/main.go
@@ -1,14 +1,14 @@
 package main
 
-import {
+import (
     "net/http"
-}
+)
 
 func init() {
     http.HandleFunc("/", redirect)
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, r.url.RawPath + r.url.RawQuery, http.StatusMovedPermanently)
+    http.Redirect(w, r, r.URL.RawPath + r.URL.RawQuery, http.StatusMovedPermanently)
 }
 
