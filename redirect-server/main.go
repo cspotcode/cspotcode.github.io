81c011b4802b08b4b6facd831e263c6b8c53cb73
Andrew Bradley <cspotcode@gmail.com>
2016-12-23 14:24:52 -0500

    Adds explicit domain name to redirect URLs

--- redirect-server/main.go
+++ redirect-server/main.go
@@ -9,6 +9,6 @@ func init() {
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, r.URL.RawPath + r.URL.RawQuery, http.StatusMovedPermanently)
+    http.Redirect(w, r, "https://cspotcode.com/" + r.URL.RawPath + r.URL.RawQuery, http.StatusMovedPermanently)
 }
 
