8bea0d4eb5bf3d98aa2fca353a175ce383b2545a
Andrew Bradley <cspotcode@gmail.com>
2016-12-24 00:10:43 -0500

    Removes unnecessary logging

--- redirect-server/src/redirect-server/main.go
+++ redirect-server/src/redirect-server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "net/http"
-    "time"
     "google.golang.org/appengine"
     "google.golang.org/appengine/datastore"
 )
@@ -10,13 +9,6 @@ import (
 type RedirectConfig struct {
     SourceDomain string
     TargetDomain string
-}
-
-type Log struct {
-    Time int32
-    SourceDomain string
-    TargetDomain string
-    Host string
 }
 
 var redirect *RedirectConfig
@@ -35,20 +27,8 @@ func getRedirectConfig(r *http.Request) {
     datastore.Get(ctx, k, redirect)
 }
 
-func writeLog(r *http.Request, l *Log) {
-    ctx := appengine.NewContext(r)
-    k := datastore.NewKey(ctx, "log", "mostrecent", 0, nil)
-    datastore.Put(ctx, k, l)
-}
-
 func onRequest(w http.ResponseWriter, r *http.Request) {
     getRedirectConfig(r)
-    writeLog(r, &Log{
-        Time: int32(time.Now().Unix()),
-        SourceDomain: redirect.SourceDomain,
-        TargetDomain: redirect.TargetDomain,
-        Host: r.Host,
-    })
     if(r.Host == redirect.SourceDomain) {
         redirectTo := "https://" + redirect.TargetDomain + r.URL.Path
         if(r.URL.RawQuery != "") {
